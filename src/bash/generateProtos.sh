1bb4a48813fd9326d25c2e3c880e82882349afc8
Tom Swirly <tom@swirly.com>
2012-02-20 17:59:26 -0500

    Get rid of Java from the proto build

--- src/bash/generateProtos.sh
+++ src/bash/generateProtos.sh
@@ -2,14 +2,12 @@
 
 ROOT=/development/mop
 
-JAVA_ROOT=$ROOT/gen/java
 PY_ROOT=$ROOT/gen/py
 PROTO_ROOT=$ROOT/src/proto
 
-mkdir -p "$JAVA_ROOT" "$PY_ROOT"
+mkdir -p "$PY_ROOT"
 
 protoc \
  --proto_path="$PROTO_ROOT" \
- --java_out="$JAVA_ROOT" \
  --python_out="$PY_ROOT" \
  "$PROTO_ROOT/"*/*/*.proto
