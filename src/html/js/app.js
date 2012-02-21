861546cbff76543f40dd26b87d8127f167ebda27
Andrew Burgess <andrew@deceptacle.com>
2012-02-21 00:08:29 -0500

    Started setting up some wireframes for the project structure

--- src/html/js/app.js
+++ src/html/js/app.js
@@ -1,5 +1,55 @@
-window.addEventListener('load', function () {
-    document.addEventListener('deviceready', function () {
-        alert("PhoneGap is now loaded!");
-    }, false);
-}, false);
+//Initialize application pages
+
+$("#start").live("pageinit", function(event) {
+	var controller = new StartController();
+	controller.initialize();
+});
+
+$("#dashboard").live("pageinit", function(event) {
+	
+});
+
+//Controller to handle the startup page
+var StartController = function() {
+	var self = this;
+	
+	var page = $("#start");
+	var txtUsername = $("#start").find("#username");
+	var txtPassword = $("#start").find("#password");
+	var btnLogin = $("#start").find("#login");
+	
+	self.initialize = function() {
+		btnLogin.click(self.btnLoginClicked);
+	};
+	
+	self.btnLoginClicked = function(event) {
+		event.preventDefault();
+		
+		if (self.validateLogin()) {
+			self.login();
+		}
+	};
+	
+	self.validateLogin = function() {
+		var valid = true;
+		if (txtUsername.val().length == 0) {
+			txtUsername.css("border-color", "#f00");
+			valid = false;
+		} else {
+			txtUsername.css("border-color", "");
+		}
+			
+		if (txtPassword.val().length == 0) {
+			txtPassword.css("border-color", "#f00");
+			valid = false;
+		} else {
+			txtPassword.css("border-color", "");
+		}
+		
+		return valid;
+	};
+	
+	self.login = function() {
+		$.mobile.changePage($("#dashboard"));
+	};
+};
