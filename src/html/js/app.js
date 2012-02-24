07cfa75c1c545ad3b2ed815c79d2503b578a2913
Andrew Burgess <andrew@deceptacle.com>
2012-02-23 22:15:19 -0500

    Set up various object states

--- src/html/js/app.js
+++ src/html/js/app.js
@@ -1,12 +1,33 @@
+var ApplicationController = function() {
+	var self = this;
+	
+	this.currentUser = new User();
+};
+
+var User = function() {
+	var self = this;
+	
+	this.userState = UserState.NONE;
+	this.bidAskState = BidAskState.NONE;
+	this.negotiationState = NegotiationState.NONE;
+	this.paymentState = PaymentState.NONE;
+	
+	this.loggedIn = false;
+};
+
 //Initialize application pages
 
 $("#start").live("pageinit", function(event) {
+	if (Application.currentUser.userState === UserState.LOGGED_IN)
+		$.mobile.changePage("#dashboard");
+
 	var controller = new StartController();
 	controller.initialize();
 });
 
 $("#dashboard").live("pageinit", function(event) {
-	
+	if (Application.currentUser.userState !== UserState.LOGGED_IN)
+		$.mobile.changePage("#start");
 });
 
 //Controller to handle the startup page
@@ -32,14 +53,14 @@ var StartController = function() {
 	
 	self.validateLogin = function() {
 		var valid = true;
-		if (self.txtUsername.val().length == 0) {
+		if (self.txtUsername.val().length === 0) {
 			self.txtUsername.css("border-color", "#f00");
 			valid = false;
 		} else {
 			self.txtUsername.css("border-color", "");
 		}
 			
-		if (self.txtPassword.val().length == 0) {
+		if (self.txtPassword.val().length === 0) {
 			self.txtPassword.css("border-color", "#f00");
 			valid = false;
 		} else {
@@ -50,6 +71,41 @@ var StartController = function() {
 	};
 	
 	self.login = function() {
+		//TODO: Authenticate with server
+		Application.currentUser.userState = UserState.LOGGED_IN;
 		$.mobile.changePage($("#dashboard"));
 	};
 };
+
+var UserState = {
+	NONE: {value: 0, name: "No State"},
+	LOGGED_IN: {value: 1, name: "Logged In"},
+	FAILURE_LOGIN: {value: 2, name: "Invalid Username/Password"},
+	FAILURE_NAME: {value: 3, name: "No Name Exists"},
+	FAILURE_NETWORK: {value: 4, name: "No Network Connectivity"},
+	FAILURE_SERVER: {value: 5, name: "Server Is Unreachable"},
+	FAILURE_ERROR: {value: 6, name: "Server Error"}
+};
+
+var BidAskState = {
+	NONE: {value: 0, name: "No Offer"},
+	BID: {value: 1, name: "User Offering To Buy Parking Space"},
+	ASK: {value: 2, name: "User Offering To Sell Parking Space"}
+};
+
+var NegotiationState = {
+	NONE: {value: 0, name: "No Negotiation"},
+	LOCATING: {value: 1, name: "User Is Setting Location For Offer"},
+	SETTING: {value: 2, name: "Location Set, Setting Price/Time"},
+	WAITING: {value: 3, name: "User Initiated Offer, No Replies"},
+	NEGOTIATING: {value: 4, name: "Tentatively Accepted Offer, Negotiating Terms"},
+	AGREEMENT: {value: 5, name: "User And Other Party Agreed To Match Bid/Ask"},
+	DELIVERED: {value: 6, name: "User Delivered, Other Party Has Not Yet"}
+};
+
+var PaymentState = {
+	NONE: {value: 0, name: "No Payment"},
+	PAYMENT: {value: 0, name: "Payment"}
+};
+
+var Application = new ApplicationController();
