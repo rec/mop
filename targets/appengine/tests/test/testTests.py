336dbfa876985809ffab44ecdba130abbff3b117
Tom Swirly <tom@swirly.com>
2012-02-24 19:26:32 -0500

    A broken test of our stuff

--- targets/appengine/tests/test/testTests.py
+++ targets/appengine/tests/test/testTests.py
@@ -1,29 +1,14 @@
 from google.appengine.api import memcache
-from google.appengine.ext import db
+from google.appengine.ext import ndb
 from google.appengine.ext import testbed
 
 import testCase
 
-import mop.store.PhoneNumber
+from mop.store import PhoneNumber
 
-class TestModel(db.Model):
-  """A model class used for testing."""
-  number = db.IntegerProperty(default=42)
-  text = db.StringProperty()
-
-class TestEntityGroupRoot(db.Model):
+class TestEntityGroupRoot(ndb.Model):
   """Entity group root"""
   pass
-
-def GetEntityViaMemcache(entity_key):
-  """Get entity from memcache if available, from datastore if not."""
-  entity = memcache.get(entity_key)
-  if entity is not None:
-    return entity
-  entity = TestModel.get(entity_key)
-  if entity is not None:
-    memcache.set(entity_key, entity)
-  return entity
 
 class DemoTestCase(testCase.TestCase):
   def _initStubs(self, testbed):
@@ -31,23 +16,8 @@ class DemoTestCase(testCase.TestCase):
     testbed.init_memcache_stub()
 
   def testInsertEntity(self):
-    TestModel().put()
-    self.assertEqual(1, len(TestModel.all().fetch(2)))
-
-  def testFilterByNumber(self):
-    root = TestEntityGroupRoot(key_name="root")
-    TestModel(parent=root.key()).put()
-    TestModel(number=17, parent=root.key()).put()
-    query = TestModel.all().ancestor(root.key()).filter('number =', 42)
-    results = query.fetch(2)
-    self.assertEqual(1, len(results))
-    self.assertEqual(42, results[0].number)
-
-  def testGetEntityViaMemcache(self):
-    entity_key = str(TestModel(number=18).put())
-    retrieved_entity = GetEntityViaMemcache(entity_key)
-    self.assertNotEqual(None, retrieved_entity)
-    self.assertEqual(18, retrieved_entity.number)
+    PhoneNumber.PhoneNumber().put()
+    # self.assertEqual(1, len(TestModel.all().fetch(2)))
 
 if __name__ == '__main__':
     unittest.main()
