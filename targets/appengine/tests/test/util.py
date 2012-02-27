3f5ade249740c108c082f71188999669b9d94ed2
Tom Swirly <tom@swirly.com>
2012-02-27 18:23:39 -0500

    Rename finished

--- targets/appengine/tests/test/util.py
+++ targets/appengine/tests/test/util.py
@@ -4,7 +4,7 @@ from google.appengine.ext import ndb
 
 from mop.model import PaymentMethod
 from mop.model import PhoneNumber
-from mop.util import modelDict
+from mop.util import ModelDict
 
 PHONE_DICT = {'country':'USA', 'areaCode': '212', 'localNumber': '5551212'}
 PAY_DICT = {'category': 'VISA', 'billingPhoneNumber': PHONE_DICT}
@@ -13,45 +13,44 @@ PAY_DICT = {'category': 'VISA', 'billingPhoneNumber': PHONE_DICT}
 class RemoveNonesTestCase(testCase.TestCase):
   def testSimple(self):
     d = {'foo' : 1, 'bar': None}
-    modelDict.removeNones(d)
+    ModelDict.removeNones(d)
     self.assertEqual(d, {'foo' : 1})
 
   def testSubdict(self):
     d = {'foo' : {'bar': None}}
-    modelDict.removeNones(d)
+    ModelDict.removeNones(d)
     self.assertEqual(d, {'foo' : {}})
 
   def testComplex(self):
     d = {'foo' : {'bar': None, 'baz': False, 'bang': 0},
          'bing': None}
-    r = modelDict.removeNones(d)
+    r = ModelDict.removeNones(d)
     self.assertEqual(d, {'foo' : {'baz': False, 'bang': 0}})
 
 
 class ToDictTestCase(testCase.TestCase):
   def testSimple(self):
     number = PhoneNumber.PhoneNumber(**PHONE_DICT)
-    d = modelDict.toDict(number)
+    d = ModelDict.toDict(number)
     self.assertEqual(d, PHONE_DICT)
 
   def testComplex(self):
     phone = PhoneNumber.PhoneNumber(**PHONE_DICT)
     pay = PaymentMethod.PaymentMethod(category='VISA',
                                       billingPhoneNumber=phone)
-    d = modelDict.toDict(pay)
+    d = ModelDict.toDict(pay)
     self.assertEqual(d, PAY_DICT)
 
 
 class ToModelTestCase(testCase.TestCase):
   def testSimple(self):
     number = PhoneNumber.PhoneNumber()
-    modelDict.toModel(number, PHONE_DICT)
+    ModelDict.toModel(number, PHONE_DICT)
     self.assertEqual(number, PhoneNumber.PhoneNumber(**PHONE_DICT))
 
   def testComplex(self):
     pay = PaymentMethod.PaymentMethod()
-    modelDict.toModel(pay, PAY_DICT)
+    ModelDict.toModel(pay, PAY_DICT)
     phone = PhoneNumber.PhoneNumber(**PHONE_DICT)
-    self.assertEqual(pay,
-                     PaymentMethod.PaymentMethod(category='VISA',
-                                                 billingPhoneNumber=phone))
+    p2 = PaymentMethod.PaymentMethod(category='VISA', billingPhoneNumber=phone)
+    self.assertEqual(pay, p2)
